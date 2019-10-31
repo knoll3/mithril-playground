8daa2c8bb1f0b77a57636a5c1461fe99ee56557a
Kyle Noll <kyle.noll@prases.com>
2019-10-30 23:45:10 -0500

    Stopping point on clock

--- src/view/components/date-time-picker/clock/index.js
+++ src/view/components/date-time-picker/clock/index.js
@@ -2,8 +2,80 @@
 import m from "mithril";
 import styles from "./index.scss";
 
+function globalMouseActions(vnode) {
+  return {
+    onmousemove: function(e) {
+      if (vnode.state.mouse.down) {
+        // force return if mouse not down
+        if (e.buttons === 0) {
+          vnode.state.mouse.down = false;
+          return;
+        }
+
+        // get clock position
+        const clock = vnode.dom;
+        const clockLeft = clock.getBoundingClientRect().left;
+        const clockTop = clock.getBoundingClientRect().top;
+        const clockPos = {
+          x: clockLeft + clock.offsetWidth / 2,
+          y: clockTop + clock.offsetHeight / 2
+        };
+
+        // get mouse position
+        const mousePos = {
+          x: e.clientX,
+          y: e.clientY
+        };
+
+        // calculate the angle
+        const deltaX = mousePos.x - clockPos.x;
+        const deltaY = mousePos.y - clockPos.y;
+        const angle = 180 - Math.atan2(deltaX, deltaY) * (180 / Math.PI);
+
+        // apply angle to the element
+        vnode.state.mouse.target.style.transform = "rotateZ(" + angle + "deg)";
+        vnode.state.mouse.target.angle = angle;
+      }
+    },
+    onmouseup: function(e) {
+      vnode.state.mouse.down = false;
+    }
+  };
+}
+
 export default {
+  oninit: function(vnode) {
+    vnode.state.mouse = {
+      down: false,
+      target: document
+    };
+    Object.assign(document, globalMouseActions(vnode));
+  },
+  oncreate: function(vnode) {},
   view: function(vnode) {
-    return m(".sub-component", "Hello, World");
+    return m(".clock", [
+      m(".hours-container", [
+        m(".hours", {
+          onmousedown: function(e) {
+            vnode.state.mouse.down = true;
+            vnode.state.mouse.target = e.target;
+          },
+          style: {
+            transform: "rotateZ(" + this.angle || 0 + "deg)"
+          }
+        })
+      ]),
+      m(".minutes-container", [
+        m(".minutes", {
+          onmousedown: function(e) {
+            vnode.state.mouse.down = true;
+            vnode.state.mouse.target = e.target;
+          },
+          style: {
+            transform: "rotateZ(" + this.angle || 0 + "deg)"
+          }
+        })
+      ])
+    ]);
   }
 };
--- src/view/components/date-time-picker/time-picker/index.js
+++ src/view/components/date-time-picker/time-picker/index.js
@@ -1,11 +1,12 @@
 "use strict";
 import m from "mithril";
 import styles from "./index.scss";
+import clock from "../clock";
 
 export default {
   view: function(vnode) {
     return m(".time-picker", [
-      m(".time-picker__analogue-clock"),
+      m(".time-picker__analogue-clock", [m(clock)]),
       m(".time-picker__text-clock"),
       m(".time-picker__reset")
     ]);
